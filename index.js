25d554a5bf7697e1c8e47243e020029d60afca58
Sebastian Hancock <sebastianc.hancock@gmail.com>
2022-01-10 22:30:35 -0800

    first

---
+++ index.js
@@ -0,0 +1,34 @@
+var express = require('express');        // call express
+var app = express();                 // define our app using express
+var bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3').verbose();
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+let db = new sqlite3.Database('db.sqlite3');
+let sql = 'SELECT * from myapi_digimon'
+
+
+var port = process.env.PORT || 8080;
+
+
+var router = express.Router();
+
+
+router.get('/', function (req, res) {
+    db.all(sql, [], (err, digimon) => {
+        if (err) { throw err }
+        res.json({ digimon });
+    })
+
+});
+
+app.use('/digimon', router);
+
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
+
